3f43fe6209e83f0d14da77941a5807abfad877b7
Estudiante 1 <45402024+victortejeda@users.noreply.github.com>
2022-09-03 21:38:24 -0400

    Initial Commit

---
+++ temporizador de huevo cocido/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  temporizador de huevo cocido
+//
+//  Created by Victor Tejeda on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
